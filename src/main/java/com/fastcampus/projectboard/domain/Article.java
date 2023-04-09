5757e8851de98906a05cb7c81922a41da9f3baab
jy-hwang <stratojy@gmail.com>
2023-04-09 11:53:22 +0900

    #7 - 도메인 클래스 오탈자 수정
    도메인 클래스 오탈자 수정 및  주석 추가

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -6,7 +6,7 @@ public class Article {
 
   private Long id;
   private String title; // 제목
-  private String content; // 내용
+  private String content; // 본문
   private String hashtag; // 해시태그
 
   private LocalDateTime createdAt; // 생성일시
--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -5,8 +5,8 @@ import java.time.LocalDateTime;
 public class ArticleComment {
 
   private Long id;
-  private Article article;
-  private String hashtag;
+  private Article article; // 게시글 (ID)
+  private String content; // 본문 
 
   private LocalDateTime createdAt; // 생성일시
   private String createdBy; // 생성자
