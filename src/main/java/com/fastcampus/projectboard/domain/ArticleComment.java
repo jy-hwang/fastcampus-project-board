bd0adc67842ecc2c249efafcf45cca59ad3080ee
jy-hwang <stratojy@gmail.com>
2023-04-10 19:44:09 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭되도록 설계했다.
    생성자와 'equals()', 'hashcode()' 등이 도메인 맥락에 맞게끔 상세하게 설정함.

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -1,16 +1,74 @@
 package com.fastcampus.projectboard.domain;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@ToString
+@Table(indexes  = {
+    @Index(columnList = "content"),
+    @Index(columnList = "createdAt"),
+    @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class ArticleComment {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private Article article; // 게시글 (ID)
-  private String content; // 본문 
+  
+  @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+  @Setter @Column(nullable = false, length = 500) private  String content; // 본문 
 
-  private LocalDateTime createdAt; // 생성일시
-  private String createdBy; // 생성자
-  private LocalDateTime modifiedAt; // 수정일시
-  private String modifiedBy; // 수정자
+  @CreatedDate  @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+  @CreatedBy  @Column(nullable = false, length = 100) private String createdBy; // 생성자
+  @LastModifiedDate  @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
+  @LastModifiedBy @Column(nullable = false,length = 100) private String modifiedBy; // 수정자
+  
+  protected ArticleComment() {  }
+
+  private ArticleComment(Article article, String content) {
+    this.article = article;
+    this.content = content;
+  }
+
+  public ArticleComment of(Article article, String content) {
+  return new ArticleComment(article, content);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (!(obj instanceof ArticleComment articleComment))
+      return false;
+    return id != null  && id.equals(articleComment.id);
+  }
+  
+  
+  
   
 }
