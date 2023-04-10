d90a3234c6445f0ee068249556745658e52f8b75
jy-hwang <stratojy@gmail.com>
2023-04-10 19:53:03 +0900

    #10 - jpa test 작성
    기본 crud + 연관관계 매핑과 cascading 이 잘 동작하는지 보려고 작성.
    테스트 데이터가 필요하기 때문에 이대로는 실패한다.

---
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,103 @@
+package com.fastcampus.projectboard.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import com.fastcampus.projectboard.config.JpaConfig;
+import com.fastcampus.projectboard.domain.Article;
+import static org.assertj.core.api.Assertions.*;
+import java.util.List;
+
+@DisplayName("Jpa 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+  private final ArticleRepository articleRepository;
+
+  private final ArticleCommentRepository articleCommentRepository;
+
+  protected JpaRepositoryTest(@Autowired ArticleRepository articleRepository,
+      @Autowired ArticleCommentRepository articleCommentRepository) {
+    this.articleRepository = articleRepository;
+    this.articleCommentRepository = articleCommentRepository;
+  }
+
+  @DisplayName("select 테스트")
+  @Test
+  void givenTestData_whenSelecting_thenWorksFine() {
+    // given
+
+    // when
+    List<Article> articles = articleRepository.findAll();
+
+    // then
+    assertThat(articles).isNotNull().hasSize(123);
+
+
+  }
+
+  @DisplayName("insert 테스트")
+  // @Test
+  void givenTestData_whenInserting_thenWorksFine() {
+
+    // given
+    long previousCount = articleRepository.count();
+
+    // when
+    articleRepository.save(Article.of("new article title", "new article Content", "#spring"));
+
+    // then
+    assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+
+  }
+
+  @DisplayName("update 테스트")
+  // Test
+  void givenTestData_whenUpdating_thenWorksFine() {
+
+    // given
+    Article article = articleRepository.findById(1L).orElseThrow();
+    String updatedHashTag = "#springboot";
+    article.setHashtag(updatedHashTag);
+
+    // when
+    // Article savedArticle = articleRepository.save(article);
+    /*
+     * @formatter:off
+     * 기본적으로 @DataJpaTest 어노테이션에 의해 트랜잭션이 걸려있어서 의도와는 다르게 생략되는 메서드가 종종생김.
+     * 그리고 개별 테스트 메서드 실행 후 롤백되는 구조이므로, 제대로 확인할 수 없는 문제가 생김.
+     * 그래서 save 후에 flush 작업을 강제로 해줘서 DB의 영속성을 맞춰줌.
+     * @formatter:on
+     */
+    Article savedArticle = articleRepository.saveAndFlush(article);
+
+    // then
+    assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashTag);
+
+  }
+
+
+  @DisplayName("delete 테스트")
+  //@Test
+  void givenTestData_whenDeleting_thenWorksFine() {
+
+    // given
+    Article article = articleRepository.findById(1L).orElseThrow();
+    long previousArticleCount = articleRepository.count();
+    long previousArticleCommentCount = articleCommentRepository.count();
+    int deleteCommentSize = article.getArticleComment().size();
+
+    // when
+    articleRepository.delete(article);
+
+    // then
+    assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+    assertThat(articleCommentRepository.count())
+        .isEqualTo(previousArticleCommentCount - deleteCommentSize);
+
+  }
+
+}
