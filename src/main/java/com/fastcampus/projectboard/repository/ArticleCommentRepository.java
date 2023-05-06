293aec7a8cecda318c1e9e289215e5f8b6ce1b16
jy-hwang <stratojy@gmail.com>
2023-05-06 18:07:02 +0900

    #1 - Settings Data Rest
    게시글과 댓글의 json api 를 자동으로 RestFul 하게 만들게끔  yaml 설정을 추가하고
    @RepositoryRestResource 를 각각 추가하였음.

--- src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -1,8 +1,10 @@
 package com.fastcampus.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.fastcampus.projectboard.domain.ArticleComment;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
 }
--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -1,8 +1,10 @@
 package com.fastcampus.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.fastcampus.projectboard.domain.Article;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
