c618493585d878de74c14688f0abfb8f99bbcd74
jy-hwang <stratojy@gmail.com>
2023-09-21 02:00:42 +0900

    #51 - 시큐리티 설정과 자바 대응 코드 구현
    스프링 부트 2.7 과 가이드에 맞춰서
    시큐리티 설정을 적용하고, 인증 정보를 db로부터 부르는 빈과
    패스워드 인코더 빈까지 등록
    영향 받는 ㅜ분을 서비스 코드와 컨트롤러에 반영

--- src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -1,8 +1,17 @@
 package com.fastcampus.projectboard.config;
 
+import com.fastcampus.projectboard.dto.UserAccountDto;
+import com.fastcampus.projectboard.dto.security.BoardPrincipal;
+import com.fastcampus.projectboard.repository.UserAccountRepository;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,11 +20,52 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     //@formatter:off
-    return  http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin().and()
-                .build();
+    return  http
+        .authorizeHttpRequests(
+                 auth -> auth
+                     .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                     .mvcMatchers(
+                         HttpMethod.GET,
+                         "/",
+                         "/articles",
+                         "/articles/search-hashtag"
+                     ).permitAll()
+                     .anyRequest().authenticated()
+        )
+        .formLogin().and()
+        .logout()
+          .logoutSuccessUrl("/")
+          .and()
+        .build();
     //@formatter:on
 
   }
+
+  @Bean
+  public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+    return username -> userAccountRepository
+        .findById(username)
+        .map(UserAccountDto::from)
+        .map(BoardPrincipal::from)
+        .orElseThrow(
+            () -> new UsernameNotFoundException("유저를 찾을 수 없습니다. - username : " + username));
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+
+/*
+spring boot 2.7 이상에서는 권장되지 않는 방식임.
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    // static resource, css - js
+
+    return (web) -> web.ignoring()
+        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+  }
+*/
 
 }
--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -6,14 +6,19 @@ import com.fastcampus.projectboard.config.JpaConfig;
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.UserAccount;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @DisplayName("Jpa 연결 테스트")
-@Import(JpaConfig.class)
+@Import(JpaRepositoryTest.TestJpaConfig.class)
 @DataJpaTest
 class JpaRepositoryTest {
 
@@ -105,5 +110,15 @@ class JpaRepositoryTest {
         .isEqualTo(previousArticleCommentCount - deleteCommentSize);
 
   }
+
+  @EnableJpaAuditing
+  @TestConfiguration
+  public static class TestJpaConfig {
+    @Bean
+    public AuditorAware<String> auditorAware() {
+      return () -> Optional.of("jackie");
+    }
+  }
+
 
 }
